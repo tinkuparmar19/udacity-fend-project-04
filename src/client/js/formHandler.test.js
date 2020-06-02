91240fc3758b2858e1908a53dcbf989770351da3
tinku parmar <tinkuparmar99@gmail.com>
2020-06-03 00:47:19 +0530

    add jest

---
+++ src/client/js/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from './formHandler'
+
+    describe('Test, the function "handleSubmit()" should exist' , () => {
+        test('It should return true', () => {
+            expect('handleSubmit').toBeDefined();
+        });
+    });
---
+++ src/client/js/nameChecker.test.js
@@ -0,0 +1,8 @@
+import { checkForName } from './nameChecker';
+
+describe('Test, the function "checkForName()" should exist' , () => {
+    test('It should return true', () => {
+        expect('checkForName').toBeDefined();
+    });
+});
+   
