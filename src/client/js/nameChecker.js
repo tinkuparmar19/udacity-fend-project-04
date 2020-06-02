87c87558ae64a7cac077cb2bb5e075a4ad5237b4
tinku parmar <tinkuparmar99@gmail.com>
2020-06-02 16:43:02 +0530

    initial commit

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,5 @@
+function checkForName(inputText) {
+    console.log("::: Running checkForName :::", inputText);
+}
+
+export { checkForName }
---
+++ src/server/index.js
@@ -0,0 +1,51 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const aylien = require("aylien_textapi");
+const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.post('/testing', async (req, res, next) => {
+    //console.log(req.body);
+    try {
+      var data = textapi.sentiment({
+        //'text': 'John is a very good football player!'
+        'text': req.body.theText
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+          res.send(response);
+        }
+      });
+      //res.send(mockAPIResponse)
+    } catch(error) {
+      // Passes errors into the handler
+      return next(error)
+    }
+    //res.send(returnVal);
+  })
+  
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
